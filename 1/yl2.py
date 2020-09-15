46971bad4b041a8750f2d12cfc4e2672ff8cf5f4
Anne-Mari <annemari.eensaar@gmail.com>
2020-09-15 15:36:50 +0300

    Koostasin ülesanne 1.2 lahenduse.

--- 1/yl2.py
+++ 1/yl2.py
@@ -2,4 +2,5 @@ aasta = 2016
 president = "Kersti Kaljulaid"
 lause_keskosa = ". aastal valiti Eesti presidendiks "
 lause = aasta + lause_keskosa + president
+lause = str(aasta) + lause_keskosa + president
 print(lause)
