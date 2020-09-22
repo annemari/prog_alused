e37b488b0794e5fde6f33423e1d79c3cdcd9267a
Anne-Mari <annemari.eensaar@gmail.com>
2020-09-22 11:00:24 +0300

    Koostasin ülesanne 1.3 lahenduse.

--- 1/yl3.py
+++ 1/yl3.py
@@ -1,6 +1,8 @@
+# andmed kasutaja poolt
 print("Sisestage astme alus: ")
 alus = input()
 print("Sisestage astendaja")
 astendaja = input()
+# arvutused + teisendamine
 aste = int(alus)**int(astendaja)
 print(aste)
