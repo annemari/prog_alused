e0415c84b893060b728b5680e49b262ad5ad0968
Anne-Mari <annemari.eensaar@gmail.com>
2020-10-01 10:59:35 +0300

    Koostasin ülesanne 3.1 lahenduse.

--- 3/yl3.1.py
+++ 3/yl3.1.py
@@ -1,14 +1,3 @@
-#Üldtsükkel
-for kord in range(1, 5, 1):
-    print(str(kord) + ". tere")
-    print("===================================")
-    
-    #vastupidi
-    for kord in range(1, 5, -1):
-        print(str(kord) + ". tere")
-
-    print("===================================")
-    kord = 1
-    while(kord <= 5):
-        print(str(kord) + ". tere")
-        kord = kord + 1 #kord += 1
+aratus = int(input("Sisestage mitu korda äratada: "))
+for i in range(aratus):
+    print("Tõuse ja sära!")
