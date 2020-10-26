7de7c27a93bdda27ec8dfe471e78eef894f5f3dc
Anne-Mari <annemari.eensaar@gmail.com>
2020-10-26 09:29:26 +0200

    Koostasin ülesanne 6.3 lahenduse.

--- 6/yl3.py
+++ 6/yl3.py
@@ -1,9 +1,15 @@
-def banner(reklaamlause: str):
-    return reklaamlause.upper()
+def kuu_nimi(kuu: int) -> str:
+    kuud = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni",
+            "juuli", "august", "september", "oktoober", "november",
+            "detsember"]
+    return kuud[kuu]
 
 
-kordused = int(input("Mitu korda soovite reklaamlauset kuvada? "))
-reklaamlause = input("Sisestage reklaamlause: ")
+def kuupäev_sõnena(kuu: str) -> str:
+    kuu_osad = kuu.split(".")
+    return "{}. {} {}. a".format(kuu_osad[0], kuu_nimi(int(kuu_osad[1]) - 1), kuu_osad[2])
 
-for i in range(kordused):
-    print(banner(reklaamlause))
+
+sisend_kuupaev = input("Sisesta kuupäev kujul DD.MM.YYYY: ")
+
+print(kuupäev_sõnena(sisend_kuupaev))
